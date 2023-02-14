d3c70010f8799e95db87547be363e0b705548d5b
koredeycode <koredey4u@gmail.com>
2023-02-14 22:52:09 +0000

    this

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+"""
+Square class
+"""
+
 
 class Square():
-    
+    """
+    Square class
+    """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """Documentation"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,10 +21,13 @@ class Square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """Documentation"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """Documentation"""
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
